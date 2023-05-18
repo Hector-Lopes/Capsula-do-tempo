fe52c2cfea200001d47f3187e088cb3c400ad064
hector-lopes <hector27.lopes@gmail.com>
2023-05-18 20:14:32 -0300

    feat: add mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-950">
+      <Text
+        className="text-5xl font-bold text-gray-50
+        "
+      >
+        your app!
+      </Text>
+      <StatusBar style="light" translucent />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+console.log({ styles });
